3cc55a80d65c92a11bf2136714a7250375cc8764
norbert barics <norbert.barics@gmail.com>
2017-05-30 23:22:44 +0200

    refactored controller and added edit menu

--- src/main/java/com/greenfox/webtodo/controller/TodoController.java
+++ src/main/java/com/greenfox/webtodo/controller/TodoController.java
@@ -1,30 +1,63 @@
 package com.greenfox.webtodo.controller;
 
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+
 import com.greenfox.webtodo.model.Todo;
 import com.greenfox.webtodo.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
+@RequestMapping("/todo")
 public class TodoController {
 
   @Autowired
   TodoRepository todoRepo;
 
-  @RequestMapping(value = "/todo", method = RequestMethod.GET)
+  @RequestMapping(value = "/")
   public String todo(Model model) {
     model.addAttribute("todolist", todoRepo.findAll());
     return "index";
   }
 
-  @RequestMapping(value = "/addTodo", method = RequestMethod.POST)
-  public String addTodo(@RequestParam ( name = "todo") String todo){
-    todoRepo.save(new Todo(todo));
-  return "redirect:/";
+  @RequestMapping(value = "/addtodo")
+  public String addTodo(@RequestParam(name = "newTodo") String newTodo) {
+    todoRepo.save(new Todo(newTodo));
+    return "redirect:/todo/";
+  }
+
+  @RequestMapping(value = "/deleteTodo")
+  public String deleteTodo(@RequestParam(name = "delete") long id) {
+    todoRepo.delete(id);
+    return "redirect:/todo/";
+  }
+
+  @RequestMapping(value = "/{id}/edit")
+  public String edit(Model model, @PathVariable("id") Long id) {
+    model.addAttribute("todo", todoRepo.findOne(id));
+    return "editTodo";
+  }
+
+  @RequestMapping(value = "/{id}/edit/update")
+  public String finishedEdit(@PathVariable("id") Long id, @RequestParam("text") String text,
+      @RequestParam(value = "urgent", defaultValue = "unchecked") String urgent,
+      @RequestParam(value = "done", defaultValue = "unchecked") String done) {
+
+    Todo todo = todoRepo.findOne(id);
+    todo.setText(text);
+    boolean isUrgent = urgent.equals("checked");
+    todo.setUrgent(isUrgent);
+    boolean isDone = done.equals("checked");
+    todo.setDone(isDone);
+
+    todoRepo.save(todo);
+
+    return "redirect:/todo/";
   }
 }
