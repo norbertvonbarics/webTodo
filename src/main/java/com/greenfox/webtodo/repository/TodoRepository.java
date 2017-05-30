5ba0f6228f37eab89f3fabbe35c8d077a9ab8d32
norbert barics <norbert.barics@gmail.com>
2017-05-30 16:03:29 +0200

    added interface sort method

--- src/main/java/com/greenfox/webtodo/repository/TodoRepository.java
+++ src/main/java/com/greenfox/webtodo/repository/TodoRepository.java
@@ -1,8 +1,9 @@
 package com.greenfox.webtodo.repository;
 
 import com.greenfox.webtodo.model.Todo;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TodoRepository extends CrudRepository<Todo, Long> {
-
+  public List<Todo> findFirst10ByOrderByTimestampDesc();
 }
